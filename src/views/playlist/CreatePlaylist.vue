136f890448008fd88b25a5f2285fffca425515a4
Hosea <hoseacorpuz@gmail.com>
2023-02-22 14:52:38 +0800

    createplaylist information store at firebase database

--- src/views/playlist/CreatePlaylist.vue
+++ src/views/playlist/CreatePlaylist.vue
@@ -17,10 +17,15 @@
 <script>
   import { ref } from 'vue'
   import useStorage from '@/composables/getStorage'
+  import useCollection from '@/composables/useCollection'
+  import getUser from '@/composables/getUser'
+  import { timestamp } from '@/firebase/config'
 
   export default {
     setup() {
-      const {filePath, url, uploadImage} = useStorage()
+      const { filePath, url, uploadImage } = useStorage()
+      const { error, addDoc } = useCollection('playlists')
+      const { user } = getUser()
 
       const title = ref('')
       const description = ref('')
@@ -31,7 +36,20 @@
         if (file.value) {
           // console.log(title.value, description.value, file.value)
           await uploadImage(file.value)
-          console.log('image uploaded, url:' , url.value)
+          await addDoc ({
+            title: title.value,
+            description: description.value,
+            userId: user.value.uid,
+            userName: user.value.displayName,
+            coverUrl: url.value,
+            filePath: filePath.value,
+            songs: [],
+            createAt: timestamp()
+          })
+          if (!error.value) {
+            console.log('playlist created!')
+          }
+          // console.log('image uploaded, url:' , url.value)
         }
       }
   
